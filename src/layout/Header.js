1b3fe9cd53c53310c6a3db225a7735ff04fa3d61
farhan ahzan <farhan98ahzan@gmail.com>
2021-03-24 18:29:33 +0530

    commit

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { FaSearch } from "react-icons/fa";
+import "./header.css";
 const Header = () => {
-  return <div className="header">Header</div>;
+  return <div className="header">
+    <div className="header__logo">
+      <img src={process.env.PUBLIC_URL + '/assets/logo.png'} alt="logo" className="logo"/>
+    </div>
+
+    <input type="checkbox" id="check"/>
+    
+    <div className="header__searchbar">
+      <input type="text" placeholder="Search"/>
+      <label for="check"><i><FaSearch/></i></label>
+    </div>
+    
+  </div>;
 };
 
 export default Header;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,7 +1,71 @@
-import React from "react";
-import "./Home.css";
+import React from 'react';
+import './Home.css';
+import Header from '../../layout/Header';
+import labs from '../../assets/labs.svg';
+import lessons from '../../assets/lessons.svg';
+import games from '../../assets/games.svg';
+import papers from '../../assets/papers.svg';
+
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Header />
+      <section className="home__content">
+        <h2 className="home__content__heading">
+          The science of today is the technology of tomorrow
+        </h2>
+        <div className="home__content__background">
+          {/*  <img src={process.env.PUBLIC_URL + '/assets/homeback.svg'} alt="background" className="background"/>*/}
+          <div className="home__content__background__item item-1">
+            <div className="home__content__background__item__column column-1">
+              <p className="column-1__heading">LABS</p>
+              <p className="column-1__content">
+                Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{' '}
+              </p>
+            </div>
+            <div className="home__content__background__item__column column-2">
+              <img className="column-2__image" src={labs} alt="labs" />
+            </div>
+          </div>
+          <div className="home__content__background__item item-2">
+          <div className="home__content__background__item__column column-1">
+              <p className="column-1__heading">LESSONS</p>
+              <p className="column-1__content">
+                Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{' '}
+              </p>
+            </div>
+            <div className="home__content__background__item__column column-2">
+              <img className="column-2__image" src={lessons} alt="lessons" />
+            </div>
+          </div>
+          <div className="home__content__background__item item-3">
+          <div className="home__content__background__item__column column-1">
+              <p className="column-1__heading">GAMES</p>
+              <p className="column-1__content">
+                Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{' '}
+              </p>
+            </div>
+            <div className="home__content__background__item__column column-2">
+              <img className="column-2__image" src={games} alt="games" />
+            </div>
+          </div>
+          <div className="home__content__background__item item-4">
+          <div className="home__content__background__item__column column-1">
+              <p className="column-1__heading">PAPERS</p>
+              <p className="column-1__content">
+                Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{' '}
+              </p>
+            </div>
+            <div className="home__content__background__item__column column-2">
+              <img className="column-2__image" src={papers} alt="papers" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
