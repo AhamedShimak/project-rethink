dfc30b8abdf9b030522b32d04831df307b9c778d
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-03-25 10:50:29 +0530

    ad and lab refined

--- src/layout/advertisement/Ad.js
+++ src/layout/advertisement/Ad.js
@@ -1,11 +1,32 @@
 import React from "react";
-import './ad.css'
-import ad from '../../assets/ad.jpg'
+import "./ad.css";
+import ad from "../../assets/ad.jpg";
+import ad2 from "../../assets/ad2.jpg";
+import ad3 from "../../assets/ad3.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Ad = () => {
-  return <div className="ad">
-<img className='ad__image' src={ad} alt="advertisment here"/>
-  </div>;
+  return (
+    <Carousel
+      autoPlay="true"
+      interval="2000"
+      infiniteLoop="true"
+      showThumbs={false}>
+      <div>
+        <img src={ad} />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src={ad2} />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src={ad3} />
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
+  );
 };
 
 export default Ad;
