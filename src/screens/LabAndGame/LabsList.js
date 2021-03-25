d4e1954d1a549b52fe7689eff6b37509704d000d
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-03-25 12:11:55 +0530

    layout done

--- src/screens/LabAndGame/LabsList.js
+++ src/screens/LabAndGame/LabsList.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Lab.css";
+import gameImg from "../../assets/gameimg.png";
 import { BsGridFill } from "react-icons/bs";
 import { BiAtom } from "react-icons/bi";
 import { IoFlask } from "react-icons/io5";
 import { MdFunctions } from "react-icons/md";
-=======
-import React from 'react';
-import './Lab.css';
-import gameImg from '../../assets/gameimg.png';
-import { BsGridFill } from 'react-icons/bs';
-import { BiAtom } from 'react-icons/bi';
-import { IoFlask } from 'react-icons/io5';
-import { MdFunctions } from 'react-icons/md';
->>>>>>> 81fa766c5dedb5cf44ef1891e578500ead249fab
 
 const Lab = () => {
   return (
@@ -24,7 +15,7 @@ const Lab = () => {
           amet, consectetur adipisicing elit. Excepturi{" "}
         </p>
       </div>
-<<<<<<< HEAD
+
       <div className="lab__container">
         <div className="lab__ellipses">
           <div className="lab__ellipses__ellipse ellipse-1">
@@ -57,41 +48,40 @@ const Lab = () => {
           iure esse omnis magni tempore nihil exercitationem repellendus impedit
           nulla quo fugit optio soluta, voluptates sequi!
         </div>
-=======
 
-      <div className="lab__ellipses">
-        <div className="lab__ellipses__ellipse">
-          <span>
-            <BsGridFill />
-          </span>
+        <div className="lab__ellipses">
+          <div className="lab__ellipses__ellipse">
+            <span>
+              <BsGridFill />
+            </span>
+          </div>
+          <div className="lab__ellipses__ellipse">
+            <span>
+              <BiAtom />
+            </span>
+          </div>
+          <div className="lab__ellipses__ellipse">
+            <span>
+              <IoFlask />
+            </span>
+          </div>
+          <div className="lab__ellipses__ellipse">
+            <span>
+              <MdFunctions />
+            </span>
+          </div>
         </div>
-        <div className="lab__ellipses__ellipse">
-          <span>
-            <BiAtom />
-          </span>
+        <div className="lab__content">
+          <div className="lab__content__item">
+            <img
+              className="lab__content__item__image"
+              src={gameImg}
+              alt="gameImg"
+            />
+            <p className="lab__content__item__title">string dogy yalu strong</p>
+            {/**2 lines maximum */}
+          </div>
         </div>
-        <div className="lab__ellipses__ellipse">
-          <span>
-            <IoFlask />
-          </span>
-        </div>
-        <div className="lab__ellipses__ellipse">
-          <span>
-            <MdFunctions />
-          </span>
-        </div>
-      </div>
-      <div className="lab__content">
-        <div className="lab__content__item">
-          <img
-            className="lab__content__item__image"
-            src={gameImg}
-            alt="gameImg"
-          />
-          <p className="lab__content__item__title">string dogy yalu strong</p>{/**2 lines maximum */}
-        </div>
-       
->>>>>>> 81fa766c5dedb5cf44ef1891e578500ead249fab
       </div>
     </div>
   );
