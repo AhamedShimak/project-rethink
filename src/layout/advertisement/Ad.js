2ec39a08a030afbcb591f5af6afe1bb9cb91e421
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-05-04 11:51:58 +0530

    CHANGEad size

--- src/layout/advertisement/Ad.js
+++ src/layout/advertisement/Ad.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import Pagination from "./Pagination";
 import "./ad.css";
 import ad from "../../assets/ad.jpg";
 import ad2 from "../../assets/ad2.jpg";
@@ -6,28 +9,71 @@ import ad3 from "../../assets/ad3.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const styles = {
+  root: {
+    position: "relative",
+  },
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: "#fff",
+  },
+  slide1: {
+    backgroundColor: "#FEA900",
+  },
+  slide2: {
+    backgroundColor: "#B3DC4A",
+  },
+  slide3: {
+    backgroundColor: "#6AC0FF",
+  },
+};
 const Ad = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleChangeIndex = (index) => {
+    setIndex(index);
+  };
   return (
     <div className="ad">
-      <Carousel
+      <div style={styles.root}>
+        <AutoPlaySwipeableViews
+          interval={2000}
+          index={index}
+          onChangeIndex={handleChangeIndex}>
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>
+            <img className="ad_img" src={ad} alt="slide1" />
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>
+            <img className="ad_img" src={ad2} alt="slide2" />
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide3)}>
+            <img className="ad_img" src={ad3} alt="slide3" />
+          </div>
+        </AutoPlaySwipeableViews>
+        <Pagination dots={3} index={index} onChangeIndex={handleChangeIndex} />
+      </div>
+      {/* <Carousel
         autoPlay="true"
         interval="2000"
         infiniteLoop="true"
         showThumbs={false}
         showIndicators={false}>
         <div>
-          <img src={ad} alt='slide1'/>
+         
           <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src={ad2} alt='slide2'/>
+    
           <p className="legend">Legend 2</p>
         </div>
         <div>
-          <img src={ad3} alt='slide3'/>
+          
           <p className="legend">Legend 3</p>
         </div>
-      </Carousel>
+      </Carousel> */}
     </div>
   );
 };
