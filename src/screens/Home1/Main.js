881bf7b716492bfef1acda06ba133b0525817328
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-05-19 17:07:38 +0530

    FIX open search buig

--- src/screens/Home1/Main.js
+++ src/screens/Home1/Main.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { MdFace, MdKeyboardBackspace, MdSearch, MdSettings } from "react-icons/md";
+import {
+  MdFace,
+  MdKeyboardBackspace,
+  MdSearch,
+  MdSettings,
+} from "react-icons/md";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
 import "./Home.css";
 
 const Main = () => {
   const [searchActive, setSearchActive] = useState(false);
+  useEffect(() => {
+    console.log("hmm");
+  }, [searchActive]);
   return (
     <div className="home__main">
-
       <div className="home__logo">
-
         <Link to="/">
-          <div >
+          <div>
             {/* <ReactSVG src="svg.svg" /> */}
             <img
               src={process.env.PUBLIC_URL + "/assets/logo.svg"}
@@ -24,15 +30,12 @@ const Main = () => {
         </Link>
       </div>
 
-
-
-
-
       <div className="navigation_items">
         <div className="home__search__container">
           <div
-            className={`home__search  ${searchActive ? "home__search__display" : ""
-              } `}>
+            className={`home__search  ${
+              searchActive ? "home__search__display" : ""
+            } `}>
             <div className="home__icons home__search__back">
               <MdKeyboardBackspace
                 onClick={() => setSearchActive(!searchActive)}
@@ -43,34 +46,17 @@ const Main = () => {
             <MdSearch className=" home__icons home__searchin__icon" />
           </div>
 
-
           <div className="home__search__button">
-
             <MdSearch
               className="home__search__button_icon "
               onClick={() => setSearchActive(!searchActive)}
             />
-
           </div>
-
-
-
-
         </div>
         <div className="navigation_button">
-
-          <MdSettings
-            className="navigation__icon "
-
-          />
-
+          <MdSettings className="navigation__icon " />
         </div>
-
-
       </div>
-
-
-
     </div>
   );
 };
