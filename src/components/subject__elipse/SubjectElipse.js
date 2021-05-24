1df70db252131ab26de5fe4a739c4f1168b944c4
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-05-24 10:07:18 +0530

    CHANGE home UI

--- src/components/subject__elipse/SubjectElipse.js
+++ src/components/subject__elipse/SubjectElipse.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./SubjectElipse.css";
-const SubjectElipse = ({ icon, title, active }) => {
+const SubjectElipse = ({ icon, title, active, bottomHeading }) => {
   return (
-    <div className={`subject__ellipse ${active ? "elipse__active" : ""}`}>
-      <span className="subject__ellipse__icon">{icon}</span>
-      <h5>{title}</h5>
+    <div className="subject__ellipse__container">
+      <div className={`subject__ellipse ${active ? "elipse__active" : ""}`}>
+        <span className="subject__ellipse__icon">{icon}</span>
+
+        {bottomHeading ? "" : <h5>{title}</h5>}
+      </div>
+      {bottomHeading ? <h5 className="heading__bottom">{title}</h5> : ""}
     </div>
   );
 };
--- src/screens/Home1/HomeSection.js
+++ src/screens/Home1/HomeSection.js
@@ -5,13 +5,64 @@ import labs from "../../assets/labs.svg";
 import lessons from "../../assets/lessons.svg";
 import games from "../../assets/games.svg";
 import papers from "../../assets/papers.svg";
+import SubjectElipse from "../../components/subject__elipse/SubjectElipse";
 import { useHistory } from "react-router";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { GiCelebrationFire, GiNewspaper, GiTeacher } from "react-icons/gi";
 const HomeSection = () => {
   let history = useHistory();
   return (
     <>
       <div className="home__content__container">
         {/*  <img src={process.env.PUBLIC_URL + '/assets/homeback.svg'} alt="container" className="container"/>*/}
+        <h3>Our services</h3>
+        <div className="home__content__container__subServices">
+          <div>
+            <SubjectElipse
+              icon={<BsFillPeopleFill />}
+              title="Seminars"
+              bottomHeading={true}
+            />
+          </div>
+          <div>
+            <SubjectElipse
+              icon={<GiTeacher />}
+              title="Find tutor"
+              bottomHeading={true}
+            />
+          </div>
+          <div>
+            <SubjectElipse
+              icon={<GiNewspaper />}
+              title="Online Exams"
+              bottomHeading={true}
+            />
+          </div>
+          <div>
+            <SubjectElipse
+              icon={<GiCelebrationFire />}
+              title="Motivation"
+              bottomHeading={true}
+            />
+          </div>
+        </div>
+        <div
+          className="home__content__container__item item-1"
+          onClick={() => history.push("/labs")}>
+          <div className="home__content__container__item__column column-1">
+            <h2 className="column-1__heading">LABS</h2>
+            <p className="column-1__content">
+              Learn with interactive simulations{" "}
+            </p>
+          </div>
+          <div className="home__content__container__item__column column-2">
+            <img
+              className="column-2__image column-2__image-1"
+              src={labs}
+              alt="labs"
+            />
+          </div>
+        </div>
         <div
           className="home__content__container__item item-2"
           onClick={() => history.push("/lessons")}>
@@ -29,31 +80,14 @@ const HomeSection = () => {
             />
           </div>
         </div>
-        <div
-          className="home__content__container__item item-1"
-          onClick={() => history.push("/labs")}>
-          <div className="home__content__container__item__column column-1">
-            <h2 className="column-1__heading">LABS</h2>
-            <p className="column-1__content">
-              Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{" "}
-            </p>
-          </div>
-          <div className="home__content__container__item__column column-2">
-            <img
-              className="column-2__image column-2__image-1"
-              src={labs}
-              alt="labs"
-            />
-          </div>
-        </div>
 
         <div
           className="home__content__container__item item-4"
           onClick={() => history.push("/papers")}>
           <div className="home__content__container__item__column column-1">
-            <h2 className="column-1__heading">PAPERS</h2>
+            <h2 className="column-1__heading">Free Courses</h2>
             <p className="column-1__content">
-              Lorem ipsum dolor si adipisicing elit. Similique, aspernatur{" "}
+              Free courses by expert teachers and institutes{" "}
             </p>
           </div>
           <div className="home__content__container__item__column column-2">
@@ -64,7 +98,7 @@ const HomeSection = () => {
             />
           </div>
         </div>
-        <div
+        {/* <div
           className="home__content__container__item item-3"
           onClick={() => history.push("/games")}>
           <div className="home__content__container__item__column column-1">
@@ -78,6 +112,25 @@ const HomeSection = () => {
               alt="games"
             />
           </div>
+        </div> */}
+        <h3>Coming soon..</h3>
+        <div className="home__content__container__soon">
+          <div>
+            <SubjectElipse
+              icon={<BsFillPeopleFill />}
+              title="AI exams"
+              bottomHeading={true}
+            />
+          </div>
+          <div>
+            <SubjectElipse
+              icon={<BsFillPeopleFill />}
+              title="Games"
+              bottomHeading={true}
+            />
+          </div>
+          {/* <div>Find tutor</div>
+          <div>Motivation</div> */}
         </div>
       </div>
     </>
