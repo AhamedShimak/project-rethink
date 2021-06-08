a5b234bc46b15fde453b91b6a9e11c00eb0bcf9b
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-06-08 13:58:21 +0530

    API changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-// export const laptonApi = "https://lapton-api.herokuapp.com";
-export const laptonApi = "http://localhost:5000";
+export const laptonApi = "https://lapton-api.herokuapp.com";
+// export const laptonApi = "http://localhost:5000";
 export const clientApi = "https://laptontest.tk";
 // export const clientApi = "http://localhost:3000";
