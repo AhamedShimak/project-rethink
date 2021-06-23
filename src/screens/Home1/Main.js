924992247ac76d513f25b3c8fe3278698742a67a
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-06-23 12:01:39 +0530

    ADD search home

--- src/screens/Home1/Main.js
+++ src/screens/Home1/Main.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
 // import { MdKeyboardBackspace, MdSearch, MdSettings } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 //import { ReactSVG } from "react-svg";
 import "./Home.css";
-
+import { MdSearch, MdKeyboardBackspace } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { setSearch } from "../../actions/searchActions";
 const Main = () => {
   const [searchActive, setSearchActive] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
+    if (keyword) {
+      setSearch(dispatch, keyword);
+      history.push(`/search/?keyword=${keyword}`);
+      // setKeyword("");
+    } else {
+      history.push(history.push(history.location.pathname));
+    }
+  };
   return (
     <div className="home__main">
       <div className="home__logo">
@@ -25,26 +40,33 @@ const Main = () => {
 
       <div className="navigation_items">
         <div className="home__search__container">
-          {/* <div
+          <div
             className={`home__search  ${
               searchActive ? "home__search__display" : ""
             } `}>
-            {/* <div className="home__icons home__search__back">
+            <div className="home__icons home__search__back">
               <MdKeyboardBackspace
                 onClick={() => setSearchActive(!searchActive)}
               />
-            </div> */}
+            </div>
 
-          {/* <input placeholder="Search"></input>
-            <MdSearch className=" home__icons home__searchin__icon" /> */}
-          {/* </div> */}
+            <input
+              placeholder="Search"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+              onKeyPress={(e) => e.key === "Enter" && submitHandler(e)}></input>
+            <MdSearch
+              className=" home__icons home__searchin__icon"
+              onClick={submitHandler}
+            />
+          </div>
 
-          {/* <div className="home__search__button">
+          <div className="home__search__button">
             <MdSearch
               className="home__search__button_icon "
               onClick={() => setSearchActive(!searchActive)}
             />
-          </div> */}
+          </div>
         </div>
         {/* <div className="navigation_button">
           <MdSettings className="navigation__icon " />
