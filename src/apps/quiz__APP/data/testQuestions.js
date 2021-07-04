3f030d81ec7d24298d7147351ab744543ad2a4e3
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-07-04 22:26:35 +0530

    ADD quiz ui data

--- src/apps/quiz__APP/data/testQuestions.js
+++ src/apps/quiz__APP/data/testQuestions.js
@@ -1,6 +1,275 @@
 export const data = [
   {
     id: 1,
+    question:
+      "அணுக்கட்டமைப்பு தொடர்பாகச் செய்யப்பட்ட பின்வரும் கண்டுபிடிப்புகளைக் கருதுக.<br>I.ஒரு கதோட்டுக் கதிர்க்குழாயினுள்ளே நேர்க்கதிர்கள்<br>II. சில வகைக் கருக்களின் மூலம் உண்டாக்கப்படும் கதிர்த்தொழிற்பாடு<br>மேற்குறித்த I, II ஆகிய கண்டுபிடிப்புக்களைச் செய்த விஞ்ஞானிகள் முறையே",
+    answers: [
+      {
+        text: "ஜே.ஜே தொம்சனும் ஹென்றி பெக்ரலும்",
+        correct: false,
+      },
+      {
+        text: "ஒயிகென் கோல்ஸ்ரைனும் ரொபொட் மில்லிகனும்",
+        correct: false,
+      },
+      {
+        text: "ஹென்றி பெக்ரலும் ஒயிகென் கோல்ஸ்ரைனும்",
+        correct: false,
+      },
+      {
+        text: "ஜே. ஜே தொம்சனும் ஏர்னஸ்ட் ரதபோர்டும்",
+        correct: false,
+      },
+      {
+        text: "ஒயிகென் கோல்ஸ்ரைனும் ஹென்றி பெக்ரலும்",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "மங்கனீசு அணுவில் (Mn, Z = 25) l=0, mt = -1 என்னும் சக்திச் சொட்டெண்கள் உள்ள இலத்திரன் எண்ணிக்கைகள் முறையே",
+    answers: [
+      {
+        text: "6, 4 ஆகும்.",
+        correct: false,
+      },
+      {
+        text: "8, 12 ஆகும்.",
+        correct: false,
+      },
+      {
+        text: "8, 5 ஆகும்",
+        correct: true,
+      },
+      {
+        text: "8, 6 ஆகும்.",
+        correct: false,
+      },
+      {
+        text: "10, 5 ஆகும்.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "M ஆனது ஆவர்த்தன அட்டவணையில் உள்ள இரண்டாம் ஆவர்த்தனத்திற்குறிய ஒரு மூலகமாகும் அது இருமுனைவுத் திருப்புதிறன் உள்ள ஒரு பங்கீட்டுவலு மூலக்கூறு MCl3 ஐ உண்டாக்குகின்றது. ஆவர்த்தன அட்டவணையில் M இன் கூட்டம்",
+    answers: [
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "13",
+        correct: false,
+      },
+      {
+        text: "14",
+        correct: false,
+      },
+      {
+        text: "15",
+        correct: true,
+      },
+      {
+        text: "16",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "O, O2-, F, F-, S2-, Cl- என்னும் இனங்களின் ஆரைகள் குறையும் வரிசை",
+    answers: [
+      {
+        text: "S2- > Cl- > O2- > F- > O > F",
+        correct: true,
+      },
+      {
+        text: "S2- > Cl- > O2- > F- > F > O",
+        correct: false,
+      },
+      {
+        text: "Cl- > S2- > O2- > F- > O > F",
+        correct: false,
+      },
+      {
+        text: "Cl- > S2- > F- > O2- > O > F",
+        correct: false,
+      },
+      {
+        text: "S2- > Cl- > O2- > O > F- > F",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question:
+      "அமில K2Cr2O7 கரைசலைப் பயன்படுத்தி எதனோல் (C2H5OH) ஐ அசற்றிக் அமிலம் (CH3COOH) ஆக ஒட்சியேற்றும் தாக்கத்தில் பரிமாறப்படும் இலத்திரன்களின் மொத்த எண்ணிக்கை",
+    answers: [
+      {
+        text: "6",
+        correct: false,
+      },
+      {
+        text: "8",
+        correct: false,
+      },
+      {
+        text: "10",
+        correct: false,
+      },
+      {
+        text: "12",
+        correct: true,
+      },
+      {
+        text: "14",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question:
+      "ஒரு பெரொக்கிநைத்திரிக் அமில மூலக்கூறிற்கு (சூத்திரம் HNO4) வரையத்தக்க உறுதிறயற்ற லூயி குற்று-கோட்டுக் கட்டமைப்புகளின் எண்ணிக்கை",
+    answers: [
+      {
+        text: "1",
+        correct: false,
+      },
+      {
+        text: "2",
+        correct: true,
+      },
+      {
+        text: "3",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+      {
+        text: "5",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question:
+      "T1(K) வெப்பநிலையிலும் P1 (Pa) அமுக்கத்திலும் ஒரு விறைத்த மூடிய கொள்கலத்தில் ஓர் இலட்சிய வாயுவின் n1  மூல்கள் உள்ளன. இக்கொள்கலத்தினுள் ஒரு மேலதிக அளவு வாயுவை அனுப்பும்போது புதிய வெப்பநிலையும் அமுக்கமும் முறையே T2, P2 ஆகும். இப்போது கொள்கலத்தில் இருக்கும் வாயு மூல்களின் மொத்த எண்ணிக்கை",
+    answers: [
+      {
+        text: "(n1T1P1)/(T2P2)",
+        correct: false,
+      },
+      {
+        text: "(n1T1P2)/(T2P1)",
+        correct: true,
+      },
+      {
+        text: "(T2P2)/(n1T1P1)",
+        correct: false,
+      },
+      {
+        text: "(n1T2P2)/(T1P1)",
+        correct: false,
+      },
+      {
+        text: "(n1T2P1)/(T1P2)",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question:
+      "AX(s), A2Y(s), AZ(s) ஆகியன நீரில் அரிதாகக் கரையும் உப்புக்களாகும். 25 பாகை C இல் அவற்றின் Ksp பெறுமானங்கள் முறையே 1.6 x 10-9, 3.2 x 10-11, 9.0 x 10-12 ஆகும். 25 பாகை C இல் கற்றயன் A+(aq) இன் செறிவு குறையும் வித்தில் இவ்வுப்புகளின் மூன்று நிரம்பிய கரைசல்களின் வரிசையைப் பின்வருவனவற்றில் எது காட்டுகின்றது?",
+    answers: [
+      {
+        text: "AX(s) > A2Y(s) > AZ(s)",
+        correct: false,
+      },
+      {
+        text: "A2Y(s) > AX(s) > AZ(s)",
+        correct: true,
+      },
+      {
+        text: "AX(s) > AZ(s) > A2Y(s)",
+        correct: false,
+      },
+      {
+        text: "A2Y(s) > AZ(s) > AX(s)",
+        correct: false,
+      },
+      {
+        text: "AZ(s) > A2Y(s) > AX(s)",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question:
+      "ஓர் எசுத்தரைத் தருவதற்கு ஒரு காபொட்சிலிக் அமிலத்திற்கு ஓர் அற்ககோலுக்குமிடையே நடைபெறும் தாக்கம் தொடர்பாகப் பின்வரும் கூற்றுகளில் எது சரியானது?",
+    answers: [
+      {
+        text: "ஒட்டுமொத்தத் தாக்கமானது ஒரு காபனைல் சேர்வையின் கருநாட்டக் கூட்டல் தாக்கமாகும்.",
+        correct: false,
+      },
+      {
+        text: "அது அற்ககோல் ஒரு கருநாடியாக தொழிற்படும் தாக்கமாகும்.",
+        correct: true,
+      },
+      {
+        text: "அது காபொட்சிலிக் அமிலத்தின் O-H பிணைப்பை உடைத்துக் கொண்டு நடைபொறும் தாக்கமாகும்.",
+        correct: false,
+      },
+      {
+        text: "அது அற்ககோலின் C-O பிணைப்பை உடைத்துக் கொண்டு நடைபெறும் தாக்கமாகும்.",
+        correct: false,
+      },
+      {
+        text: "அது ஓர் அமில மூலத் தாக்கமாகும்.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "பின்வருவனவற்றுள் பிழையான கூற்றை இனம்காண்க.",
+    answers: [
+      {
+        text: "நைதரசன் இன் இலத்திரன் பெறும் சக்தி நேரானது.",
+        correct: false,
+      },
+      {
+        text: "கரைசலை நீருடன் ஐதாக்கும்போது ஒரு வெள்ளை வீழ்படிவு கிடைக்கிறது.",
+        correct: false,
+      },
+      {
+        text: "வாயுவானது ஓர் ஒட்சியேற்றக் கருவியாகவும் ஒரு தாழ்த்தும் கருவியாகவும் தொழிற்படலாம்.",
+        correct: false,
+      },
+      {
+        text: "இன் ஒரு வலுவளவு இலத்திரனினால் உணரப்படும் பயன்படு கரு ஏற்றம் (Z*) ஆனது 2 இலும் குறைவானது.",
+        correct: true,
+      },
+      {
+        text: "அலுமினியம் ஓர் உயர் வெப்பநிலைக்கு வெப்பமாக்கப்படும்போதும் N2 வாயுவை நோக்கிச் சடத்துவமானது.",
+        correct: true,
+      },
+    ],
+  },
+
+  {
+    id: 11,
     question: "Rolex is a company that specializes in what type of product?",
     answers: [
       {
@@ -22,7 +291,7 @@ export const data = [
     ],
   },
   {
-    id: 2,
+    id: 12,
     question: "When did the website `Facebook` launch?",
     answers: [
       {
@@ -44,7 +313,7 @@ export const data = [
     ],
   },
   {
-    id: 3,
+    id: 13,
     question: "When did the website `Facebook` launch?3",
     answers: [
       {
@@ -66,7 +335,7 @@ export const data = [
     ],
   },
   {
-    id: 4,
+    id: 14,
     question: "When did the website `Facebook` launch?4",
     answers: [
       {
@@ -88,7 +357,7 @@ export const data = [
     ],
   },
   {
-    id: 5,
+    id: 15,
     question: "When did the website `Facebook` launch?5",
     answers: [
       {
@@ -106,50 +375,6 @@ export const data = [
       {
         text: "2007",
         correct: false,
-      },
-    ],
-  },
-  {
-    id: 6,
-    question: "When did the website `Facebook` launch?6",
-    answers: [
-      {
-        text: "2004",
-        correct: true,
-      },
-      {
-        text: "2005",
-        correct: false,
-      },
-      {
-        text: "2006",
-        correct: false,
-      },
-      {
-        text: "2007",
-        correct: false,
-      },
-    ],
-  },
-  {
-    id: 7,
-    question: "Who played the character of harry potter in movie?7",
-    answers: [
-      {
-        text: "Johnny Deep",
-        correct: false,
-      },
-      {
-        text: "Leonardo Di Caprio",
-        correct: false,
-      },
-      {
-        text: "Denzel Washington",
-        correct: false,
-      },
-      {
-        text: "Daniel Red Cliff",
-        correct: true,
       },
     ],
   },
