90e5732e150cb8745a0cdbc6f8184d3f9facd0c8
farhanahzan <41515998+farhanahzan@users.noreply.github.com>
2021-07-27 12:17:50 +0530

    commitsuccess

--- src/screens/auth/register/SuccessRegister.js
+++ src/screens/auth/register/SuccessRegister.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Register.css";
 const SuccessRegister = () => {
   return (
     <form className="register_sign_up_form">
-      <h2 className="register_title">
+      <div className="register_sign_up_form_image">
+      <img
+                src={process.env.PUBLIC_URL + "/assets/success.png"}
+                alt="success"
+                className="register_sign_up_form_image_success"
+               
+              />
+      </div>
+      <h2 className="register_sign_up_form_title">
         {" "}
-        Thank you for registering. We will contact you soon. Best wishes from
-        Lepton
+        THANK YOU!
       </h2>
+      <p className="register_sign_up_form_text">
+       Your submission has been sent. We will contact you soon. Best wishes from Lepton
+      </p>
+      <Link to="/">
+      <button className="register_sign_up_form_button">Start Exploring</button>
+      </Link>
+      
     </form>
   );
 };
